fe6de916b67e79e50927dc74eb455bea4f24dc9f
sunyh <2474798113@qq.com>
2022-11-09 17:46:21 +0800

    .

---
+++ script.js
@@ -0,0 +1,23 @@
+
+//滚动比率
+let scrolled
+//监听滚动事件，更新滚动比率
+window.addEventListener('scroll',()=>{
+    scrolled = document.body.scrollTop / (document.body.scrollHeight - document.body.clientHeight);
+    console.log(scrolled);
+    let $x = document.querySelector(".x");
+    let $h = document.querySelector(".h");
+    let $y = document.querySelector(".y");
+    let $z = document.querySelector(".z");
+
+    $z.style.clipPath = `inset(${scrolled * 1000}% 0 0 0)`;
+    $y.style.clipPath = `inset(${(scrolled-0.2) * 1000}% 0 0 0)`;
+    $h.style.clipPath = `inset(${(scrolled-0.3) * 1000}% 0 0 0)`;
+    $x.style.clipPath = `inset(${(scrolled-0.4) * 1000}% 0 0 0)`;
+
+       
+})
+
+
+
+
